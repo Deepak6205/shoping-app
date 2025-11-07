0d8dc91105d904e845f08ca56fd4c0a4fafa4273
deepak kumar <deepak7292832956@gmail.com>
2025-11-07 18:11:53 +0530

    category section

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../../components/lalyout/Layout";
 import HeroSection from "../../components/heroSection/HeroSection";
+import Category from "../../components/category/Category";
 
 const HomePage = () => {
   return (
     <Layout>
       <HeroSection />
+      <Category />
     </Layout>
   );
 };
