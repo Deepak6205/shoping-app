4538e90f6e20930b8e8dbd6e7e582a1fb8b65d80
deepak kumar <deepak7292832956@gmail.com>
2025-11-07 19:29:05 +0530

    worked on testimonial and track

---
+++ src/components/testimonial/Testimonial.jsx
@@ -0,0 +1,90 @@
+const Testimonial = () => {
+  return (
+    <div>
+      <section className="text-gray-600 body-font mb-10">
+        {/* main  */}
+        <div className="container px-5 py-10 mx-auto">
+          {/* Heading  */}
+          <h1 className=" text-center text-3xl font-bold text-black">
+            Testimonial
+          </h1>
+          {/* para  */}
+          <h2 className=" text-center text-2xl font-semibold mb-10">
+            What our <span className=" text-pink-500">customers</span> are
+            saying
+          </h2>
+
+          <div className="flex flex-wrap -m-4">
+            {/* Testimonial 1 */}
+            <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+              <div className="h-full text-center">
+                <img
+                  alt="testimonial"
+                  className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                  src="https://people.com/thmb/w1chbqigNSl9tlxLgVK3SI5vTEY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(216x0:218x2)/will-smith-435-57-05364c08a4b44a839a5fb30de44bec2f.jpg"
+                />
+                <p className="leading-relaxed">
+                  Edison bulb retro cloud bread echo park, helvetica stumptown
+                  taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee
+                  ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut
+                  adaptogen squid fanny pack vaporware.
+                </p>
+                <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
+                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">
+                  Mr. Smith
+                </h2>
+                <p className="text-gray-500">Senior Product Designer</p>
+              </div>
+            </div>
+
+            {/* Testimonial 2 */}
+            <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+              <div className="h-full text-center">
+                <img
+                  alt="testimonial"
+                  className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                  src="https://static01.nyt.com/images/2021/03/10/world/10virus-oz-hollywood-1/10virus-oz-hollywood-1-mediumSquareAt3X.jpg"
+                />
+                <p className="leading-relaxed">
+                  Edison bulb retro cloud bread echo park, helvetica stumptown
+                  taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee
+                  ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut
+                  adaptogen squid fanny pack vaporware.
+                </p>
+                <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
+                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">
+                  Mr. Cris
+                </h2>
+                <p className="text-gray-500">UI Develeoper</p>
+              </div>
+            </div>
+
+            {/* Testimonial 3 */}
+            <div className="lg:w-1/3 lg:mb-0 p-4">
+              <div className="h-full text-center">
+                <img
+                  alt="testimonial"
+                  className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKCSlGDaGi7jj0tFTQkk111xmglm9NsU2mUBgBDEl4Yl8vWS0NAnLVLmeA3PjCxKiwp-Y&usqp=CAU"
+                />
+                <p className="leading-relaxed">
+                  Edison bulb retro cloud bread echo park, helvetica stumptown
+                  taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee
+                  ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut
+                  adaptogen squid fanny pack vaporware.
+                </p>
+                <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
+                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">
+                  Mr. Logan
+                </h2>
+                <p className="text-gray-500">CTO</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Testimonial;
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Layout from "../../components/lalyout/Layout";
 import HeroSection from "../../components/heroSection/HeroSection";
 import Category from "../../components/category/Category";
+import Track from "../../components/track/Track";
+import Testimonial from "../../components/testimonial/Testimonial";
+//import HomePageProductCard from "../../components/homePageProductCard/HomePageProductCard";
 
 const HomePage = () => {
   return (
     <Layout>
       <HeroSection />
       <Category />
+      {/* <HomePageProductCard /> */}
+      <Track/>
+      <Testimonial/>
     </Layout>
   );
 };
