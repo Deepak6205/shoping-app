78abbd4161dd3cbbe7b47f5e497252caa497ba25
deepak kumar <deepak7292832956@gmail.com>
2025-11-08 09:33:34 +0530

    work on user dashboard, admin dashboard,order details product page ...

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,32 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/home/HomePage'
 import NoPage from './pages/noPage/NoPage'
+import ProductInfo from './pages/productInfo/ProductInfo'
+import ScrollTop from './components/scrollTop/ScrollTop'
+import CartPage from './pages/cart/CartPage'
+import Signup from './pages/registration/Signup'
+import Login from './pages/registration/Login'
+import UserDashboard from './pages/user/UserDashboard'
+import AdminDashboard from './pages/admin/AdminDashboard'
+import AddProductPage from './pages/admin/AddProductPage'
+import UpdateProductPage from './pages/admin/UpdateProductPage'
 
 const App = () => {
   return (
     <div >
       <BrowserRouter>
+      <ScrollTop/>
         <Routes>
           <Route path="/" element={<HomePage/>} />
           <Route path="/*" element={<NoPage/>} />
+          <Route path="/productinfo" element={<ProductInfo/>} />
+          <Route path="/cart" element={<CartPage/>} />
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/Login" element={<Login/>} />
+          <Route path="/user-dashboard" element={<UserDashboard/>} />
+          <Route path="/admin-dashboard" element={<AdminDashboard/>} />
+          <Route path="/addproduct" element={<AddProductPage/>} />
+          <Route path="/updateproduct" element={<UpdateProductPage/>} />
         </Routes>
       
       </BrowserRouter>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,56 +1,85 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SearchBar from '../searchBar/SearchBar'
+import { Link, useNavigate } from "react-router-dom";
+import SearchBar from "../searchBar/SearchBar";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
-    const navList = (
-    <ul className='flex space-x-3 text-white font-medium text-md px-5'>
+  const user = JSON.parse(localStorage.getItem("users"));
+
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.clear("users");
+    navigate("/login");
+  };
+
+  const cartItems = useSelector((state) => state.cart);
+
+  const navList = (
+    <ul className="flex space-x-3 text-white font-medium text-md px-5 ">
       <li>
         <Link to={"/"}>Home</Link>
       </li>
-      <li>
-        <Link to={"/allProduct"}>All Product</Link>
-      </li>
-      <li>
-        <Link to={"/signup"}>signup</Link>
-      </li>
-      <li>
-        <Link to={"/"}>User</Link>
-      </li>
-      {/* admin */}
-      {/* <li>
-        <Link to={"/"}>Admin</Link>
-      </li>
-      <li>
-        Logout
-      </li> */}
 
-      {/* cart */}
       <li>
-        <Link to={"/cart"}>Cart(0)</Link>
+        <Link to={"/allproduct"}>All Product</Link>
+      </li>
+
+      {!user ? (
+        <li>
+          <Link to={"/signup"}>Signup</Link>
+        </li>
+      ) : (
+        ""
+      )}
+
+      {!user ? (
+        <li>
+          <Link to={"/login"}>Login</Link>
+        </li>
+      ) : (
+        ""
+      )}
+
+      {user?.role === "user" && (
+        <li>
+          <Link to={"/user-dashboard"}>User</Link>
+        </li>
+      )}
+
+      {user?.role === "admin" && (
+        <li>
+          <Link to={"/admin-dashboard"}>Admin</Link>
+        </li>
+      )}
+
+      {user && (
+        <li className=" cursor-pointer" onClick={logout}>
+          logout
+        </li>
+      )}
+
+      <li>
+        <Link to={"/cart"}>Cart({cartItems.length})</Link>
       </li>
     </ul>
-    )
+  );
   return (
-    <nav className='bg-pink-600 stick top-0'>
-        {/*  Main */}
-    <div className='lg:flex lg:justify-between items-center py-3 lg:px-3'>
-        {/* left */}
-        <div className='left py-3 lg:py-0'>
-            <Link to={"/"} >
-                <h2 className='font-bold text-white text-2xl text-center'>Your-BaaZaar</h2>
-            </Link>
+    <nav className="bg-pink-600 sticky top-0">
+      <div className="lg:flex lg:justify-between items-center py-3 lg:px-3 ">
+        <div className="left py-3 lg:py-0">
+          <Link to={"/"}>
+            <h2 className=" font-bold text-white text-2xl text-center">
+              Your-BaaZaar
+            </h2>
+          </Link>
         </div>
-        {/* right */}
-        <div className='right flex justify-center mb-4 lg:mb-0'>
-            {navList}
-        </div>
-        {/* searchbar */}
-        <SearchBar />
-    </div>
-    </nav>
-    
-  )
-}
 
-export default Navbar
+        <div className="right flex justify-center mb-4 lg:mb-0">{navList}</div>
+
+        <SearchBar />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
